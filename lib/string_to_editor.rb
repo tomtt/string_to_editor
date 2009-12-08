17e509db7ecdd39fd051e72a2f8025ef31af92c2
Tom ten Thij <git@tomtenthij.nl>
2009-12-08 19:13:31 +0000

    Code to write string to a temporary file and open that in an editor

---
+++ lib/string_to_editor.rb
@@ -0,0 +1,24 @@
+require 'tempfile'
+
+module OpenStringInEditor
+  def write_to_tmp_file
+    return @path_of_tmp_file_for_string_to_editor if @path_of_tmp_file_for_string_to_editor
+    Tempfile.open('ruby_string') do |tempfile|
+      tempfile.puts self
+      @path_of_tmp_file_for_string_to_editor = tempfile.path
+    end
+  end
+
+  def edit
+    if ENV['EDITOR']
+      write_to_tmp_file
+      system("#{ENV['EDITOR']} #{@path_of_tmp_file_for_string_to_editor} &")
+    else
+      raise "edit was called for a String, but the EDITOR variable was not set in ENV"
+    end
+  end
+end
+
+class String
+  include OpenStringInEditor
+end
--- spec/string_to_editor_spec.rb
+++ spec/string_to_editor_spec.rb
@@ -1,7 +1,73 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+require 'tempfile'
+require 'string_to_editor'
+
 describe "StringToEditor" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before do
+    @mock_temp_file = mock('temp file',
+                           :path => '/path/to/temp_file',
+                           :puts => nil)
+    Tempfile.stub!(:open).and_yield @mock_temp_file
+  end
+
+  describe "write_to_tmp_file" do
+    it "should open a temporary file to write to" do
+      Tempfile.should_receive(:open).with('ruby_string')
+      "bla".write_to_tmp_file
+    end
+
+    it "should write the string to the temporary file" do
+      @mock_temp_file.should_receive(:puts).with "pooh"
+      "pooh".write_to_tmp_file
+    end
+
+    it "should store the path of the temporary file in @path_of_tmp_file_for_string_to_editor" do
+      s = "honey"
+      s.write_to_tmp_file
+      s.instance_variable_get('@path_of_tmp_file_for_string_to_editor').should == '/path/to/temp_file'
+    end
+
+    it "should return the path of the temporary file" do
+      s = "honey"
+      s.write_to_tmp_file.should == '/path/to/temp_file'
+    end
+
+    it "should write to a tmp file only once" do
+      s = "mash"
+      s.write_to_tmp_file
+      Tempfile.should_not_receive(:open)
+      s.write_to_tmp_file
+    end
+
+    it "should return the path of the temporary file if it was already written to a temporary file" do
+      s = "honey"
+      s.write_to_tmp_file
+      s.write_to_tmp_file.should == '/path/to/temp_file'
+    end
+  end
+
+  describe "edit" do
+    describe "if no editor command is specified in ENV['EDITOR']" do
+      before do
+        ENV.delete('EDITOR')
+      end
+
+      it "should raise an error" do
+        lambda { "breakfast".edit }.should raise_error
+      end
+    end
+
+    describe "if ENV['EDITOR'] contains an editor command" do
+      before do
+        ENV['EDITOR'] = '/bin/editor/command'
+      end
+
+      it "should send a string to the edit command in the background" do
+        s = "sausage"
+        s.should_receive(:system).with('/bin/editor/command /path/to/temp_file &')
+        s.edit
+      end
+    end
   end
 end
